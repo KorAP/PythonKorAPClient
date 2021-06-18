d7251c2a92442e3b705da9c1da895bb4bc136e69
Marc Kupietz <kupietz@ids-mannheim.de>
2021-06-18 12:32:38 +0200

    Fix error bars in hello_world.py
    As the argument names suggest, Plotly Express uses relative values
    for error_y and error_y_minus.
    
    Change-Id: Iabfc017eb6f97b4f0329d47f7f8f0851756154a2

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -8,13 +8,14 @@ COUNTRIES = ["DE", "CH"]
 
 kcon = KorAPConnection(verbose=True)
 
-vcs = ["textType=/Zeit.*/ & pubPlaceKey=" + c + " & pubDate in " + str(y) for c in COUNTRIES for y in YEARS]
+vcs = [f"textType=/Zeit.*/ & pubPlaceKey={c} & pubDate in {y}" for c in COUNTRIES for y in YEARS]
 df = KorAPClient.ipm(kcon.frequencyQuery(QUERY, vcs))
 print(df)
 
 df['Year'] = [y for c in COUNTRIES for y in YEARS]
 df['Country'] = [c for c in COUNTRIES for y in YEARS]
+df['error_y'] = df["conf.high"] - df["ipm"]
+df['error_y_minus'] = df["ipm"] - df["conf.low"]
 
-fig = px.line(df, title=QUERY, x="Year", y="ipm", color="Country",
-              error_y="conf.high", error_y_minus="conf.low")
+fig = px.line(df, title=QUERY, x="Year", y="ipm", color="Country", error_y="error_y")
 fig.show()
